f53503208528924d58588d29594baaeb8485e2c6
Justin McCune <performancedeliveredindeed@accenture.com>
2019-04-25 23:02:51 -0700

    Set background image

---
+++ script.js
@@ -0,0 +1,13 @@
+var bgImageArr = [
+    'https://images.unsplash.com/photo-1548354911-457c10eaf236?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1386&q=80',
+    'https://images.unsplash.com/photo-1507437655600-801a83dd885c?ixlib=rb-1.2.1&auto=format&fit=crop&w=1457&q=80'
+
+];
+
+var bgNumber = Math.floor((Math.random() * 3) + 1);
+
+/*
+$(document).ready(function () {
+    $("#backgroundImage").attr('url', bgImageArr[1]);
+});
+*/
